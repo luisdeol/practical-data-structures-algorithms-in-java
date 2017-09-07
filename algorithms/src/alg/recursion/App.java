a2f8543490c5730802aa3b1c5b9a447b8a4eb3e2
Luis Felipe <luisfelipekai@gmail.com>
2017-09-06 23:00:48 -0300

    Recursive Linear and Binary Search.

--- algorithms/src/alg/recursion/App.java
+++ algorithms/src/alg/recursion/App.java
@@ -5,6 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		reduceByOne(5);
+		System.out.println(recursiveLinearSearch(new int[] { 1,2,3,4,5}, 1, 5));
+		System.out.println(recursiveBinarySearch(new int[] { 1,2,3,4,5, 6, 7, 8, 9, 10}, 0, 9, 13));
 	}
 	
 	public static void reduceByOne(int n) {
@@ -15,6 +17,28 @@ public class App {
 	}
 	
 	public static int recursiveLinearSearch(int[] a, int i, int x) {
-		return -1;
+		if (i > a.length -1)
+			return -1;
+		else if (a[i] == x)
+			return i;
+		else {
+			System.out.println("Index at " + i);
+			return recursiveLinearSearch(a, i+1, x);
+		}
+	}
+	
+	public static int recursiveBinarySearch(int[] a, int p, int r, int x) {
+		System.out.println("[ " + p + "..." + r + " ]");
+		if (p > r)
+			return -1;
+		else {
+			int q = (p+r)/2;
+			if (a[q] == x)
+				return q;
+			else if (a[q]>x)
+				return recursiveBinarySearch(a, p, q -1, x);
+			else
+				return recursiveBinarySearch(a, q + 1, r, x);
+		}
 	}
 }
