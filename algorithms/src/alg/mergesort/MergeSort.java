d95f3c8eeb9eb947f150df93db0740c75adcadfb
Luis Felipe <luisfelipekai@gmail.com>
2017-09-09 06:16:27 -0300

    Merge Sort (is not working)

---
+++ algorithms/src/alg/mergesort/MergeSort.java
@@ -0,0 +1,54 @@
+package alg.mergesort;
+
+public class MergeSort {
+
+	public static void sort(int inputArray[]) {
+		sort(inputArray, 0, inputArray.length-1);
+	}
+	
+	public static void sort(int inputArray[], int start, int end) {
+		if (end <= start){
+			return;
+		}
+		
+		int mid = (start+end)/2;
+		sort(inputArray, start, mid);
+		sort(inputArray, mid+1, end);
+		merge(inputArray,start,mid,end);
+	}
+	
+	public static void merge(int inputArray[], int start, int mid, int end) {
+		int tempArray[] = new int[end-start+1];
+		int leftSlot = start;
+		int rightSlot = mid+1;
+		int k = 0;
+		while(leftSlot <= mid && rightSlot <= end) {
+			if (inputArray[leftSlot] < inputArray[rightSlot]) {
+				tempArray[k] = inputArray[leftSlot];
+				leftSlot++;
+			}
+			else {
+				tempArray[k] = inputArray[rightSlot];
+				rightSlot++;
+			}
+			k++;
+		}
+
+		if (leftSlot <= mid) {
+			while(leftSlot <= mid) {
+				tempArray[k] = inputArray[leftSlot];
+				leftSlot++;
+				k++;
+			}
+		} else if (rightSlot <= end){
+			while(rightSlot <= end) {
+				tempArray[k] = inputArray[rightSlot];
+				rightSlot++;
+				k++;
+			}
+		}
+		for (int i1=0; i1<tempArray.length; i1++) {
+			inputArray[start+1] = tempArray[i1];
+		}
+	}
+}
