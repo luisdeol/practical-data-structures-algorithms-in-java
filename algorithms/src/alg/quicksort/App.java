0e49e68d49b90799efbff8c48f5ca3451d8a879a
Luis Felipe <luisfelipekai@gmail.com>
2017-09-10 10:11:30 -0300

    Quick Sort

--- algorithms/src/alg/quicksort/App.java
+++ algorithms/src/alg/quicksort/App.java
@@ -3,8 +3,8 @@ package alg.quicksort;
 public class App {
 
 	public static void main(String[] args) {
-		int[] inputArray = {10,7,1,3,5,8,9,6};
-		QuickSort.Partition(inputArray, 0, 7);
+		int[] inputArray = {12,81,74,43,1098,0,8,92,17,754,912,0,6,4};
+		QuickSort.QuickSort(inputArray, 0, inputArray.length-1);
 		
 		for(int i=0;i<inputArray.length;i++)
 			System.out.println(inputArray[i]);
--- algorithms/src/alg/quicksort/QuickSort.java
+++ algorithms/src/alg/quicksort/QuickSort.java
@@ -1,19 +1,31 @@
 package alg.quicksort;
 
 public class QuickSort {
-	public static void Partition(int[] inputArray, int p, int r) {
-		int i = p-1;
-		int j = p;
-		int pivot = r;
-		while(j<=r) {
-			if (inputArray[j]<=inputArray[pivot]) {
+	public static void QuickSort(int[] inputArray, int start, int end) {
+		if (start<end) {
+			int q=Partition(inputArray,start,end);
+			QuickSort(inputArray,start,q-1);
+			QuickSort(inputArray,q+1,end);
+		}
+
+	}
+	
+	public static int Partition(int[] inputArray, int start, int end) {
+		int i = start-1;
+		int pivot = inputArray[end];
+		for (int j=start;j<=end-1;j++){
+			if (inputArray[j]<=pivot) {
 				i++;
 				int temp = inputArray[j];
 				inputArray[j] = inputArray[i];
 				inputArray[i]=temp;
 				System.out.println(" J : " + j + " I: " + i);
 			}
-			j++;
-		}	
+		}
+		
+		int ival = inputArray[i+1];
+		inputArray[end]=ival;
+		inputArray[i+1]=pivot;
+		return i+1;
 	}
 }
